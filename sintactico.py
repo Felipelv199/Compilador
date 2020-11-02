8e39fc72085ded01b2a4ba26a94b07c85c83a7cf
Jose Carlos V <jcarlosv1597@gmail.com>
2020-11-01 20:11:36 -0600

    Agregue deteccion de operaciones aritmeticas y asignaciones

--- sintactico.py
+++ sintactico.py
@@ -6,10 +6,45 @@ import ply.yacc as yacc
 from compilador import tokens
 
 
+
+
+def p_expression_plus(p):
+    'factor : CteEnt OpArit CteEnt'
+
+    if p[2]=='+':
+        p[0] = p[1] + p[3]
+    if p[2]=='-':
+        p[0] = p[1] - p[3]
+    if p[2]=='*':
+        p[0] = p[1] * p[3]
+    if p[2]=='/':
+        p[0] = p[1] / p[3]
+
+def p_asignacionID(p):
+    '''factor : ID OpAsig CteEnt
+    | ID OpAsig CteReal
+    | ID OpAsig CteAlfa
+    | ID OpAsig CteLog'''
+    p[0]=p[1]
+
+def p_expression_term(p):
+     'expression : term'
+     print('valor: ' + p[1])
+     p[0] = p[1]
+
+
+def p_term_factor(p):
+     'term : factor'
+     p[0] = p[1]
+
 def p_factor_tipo(p):
     'factor : PalRes'
     p[0] = p[1]
 
+def p_factor_num(p):
+     '''factor : CteEnt
+                | CteReal '''
+     p[0] = p[1]
 
 def p_error(p):
     print("Syntax error in input!")
