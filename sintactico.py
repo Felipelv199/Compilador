3168934fdf0ff326378575310a156bc44fd0ab2a
Felipelv199 <0204021@up.edu.mx>
2020-11-01 19:30:51 -0700

    some functions deleted and renamed in sintactico.py

--- sintactico.py
+++ sintactico.py
@@ -6,45 +6,25 @@ import ply.yacc as yacc
 from compilador import tokens
 
 
-
-
-def p_expression_plus(p):
-    'factor : CteEnt OpArit CteEnt'
-
-    if p[2]=='+':
+def p_expArit(p):
+    'ExpArit : CteEnt OpArit CteEnt'
+    if p[2] == '+':
         p[0] = p[1] + p[3]
-    if p[2]=='-':
+    if p[2] == '-':
         p[0] = p[1] - p[3]
-    if p[2]=='*':
+    if p[2] == '*':
         p[0] = p[1] * p[3]
-    if p[2]=='/':
+    if p[2] == '/':
         p[0] = p[1] / p[3]
 
-def p_asignacionID(p):
-    '''factor : ID OpAsig CteEnt
+
+def p_grupoId(p):
+    '''GpoId : ID OpAsig CteEnt
     | ID OpAsig CteReal
     | ID OpAsig CteAlfa
     | ID OpAsig CteLog'''
-    p[0]=p[1]
-
-def p_expression_term(p):
-     'expression : term'
-     print('valor: ' + p[1])
-     p[0] = p[1]
-
-
-def p_term_factor(p):
-     'term : factor'
-     p[0] = p[1]
-
-def p_factor_tipo(p):
-    'factor : PalRes'
     p[0] = p[1]
 
-def p_factor_num(p):
-     '''factor : CteEnt
-                | CteReal '''
-     p[0] = p[1]
 
 def p_error(p):
     print("Syntax error in input!")
