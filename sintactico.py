c045f24b42f8a8877574cdf7d355f978ea3fddec
Felipelv199 <0204021@up.edu.mx>
2020-11-15 15:23:42 -0700

    Some error detection added to sintactico.py

--- sintactico.py
+++ sintactico.py
@@ -11,6 +11,9 @@ class sintactico:
         self.file_error.write('{:<10}|{:<30}|{:<40}|{}\n'.format(
             '', '', error_description, ''))
 
+    def print_sintactic_error(self, p, error_description):
+        print('ERROR: {} | {}'.format(p.lexer.lineno, error_description))
+
     def join_result(self, p):
         s = ''
         for i in range(1, len(p)):
@@ -20,23 +23,25 @@ class sintactico:
     def start_sintactic(self, lexer):
         def p_Prgrm(p):
             '''
-            Prgrm : constantes variables FuncProc PROGRAMA Block FIN DE PROGRAMA
+            Prgrm : constantes variables FuncProc PROGRAMA Block FIN DE PROGRAMA DOT
             '''
             p[0] = self.join_result(p)
 
         def p_Prgrm_error_Programa(p):
             '''
-            Prgrm : constantes variables FuncProc error Block FIN DE PROGRAMA
+            Prgrm : constantes variables FuncProc error Block FIN DE PROGRAMA DOT
             '''
-            print("Mala sintaxis es neceario poner Programa")
+            self.print_sintactic_error(
+                p, 'Mala sintaxis es neceario escribir Programa')
 
         def p_Prgrm_error_Fin(p):
             '''
-            Prgrm : constantes variables FuncProc PROGRAMA Block error DE PROGRAMA
-                  | constantes variables FuncProc PROGRAMA Block FIN error PROGRAMA
-                  | constantes variables FuncProc PROGRAMA Block FIN DE error
+            Prgrm : constantes variables FuncProc PROGRAMA Block error DE PROGRAMA DOT
+                  | constantes variables FuncProc PROGRAMA Block FIN error PROGRAMA DOT
+                  | constantes variables FuncProc PROGRAMA Block FIN DE error DOT
             '''
-            print("Mala sintaxis es neceario poner Fin de Programa")
+            self.print_sintactic_error(
+                p, 'Mala sintaxis es neceario escribir Fin de Programa')
 
         def p_FuncProc(p):
             '''
@@ -52,11 +57,41 @@ class sintactico:
             '''
             p[0] = self.join_result(p)
 
+        def p_Proc_error(p):
+            '''
+            Proc : error ID LPARENTHESIS Params RPARENTHESIS variables INICIO Block FIN DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO error LPARENTHESIS Params RPARENTHESIS variables INICIO Block FIN DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID error Params RPARENTHESIS variables INICIO Block FIN DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params error variables INICIO Block FIN DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params RPARENTHESIS variables error Block FIN DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params RPARENTHESIS variables INICIO Block error DE PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params RPARENTHESIS variables INICIO Block INICIO error PROCEDIMIENTO DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params RPARENTHESIS variables INICIO Block INICIO DE error DOTCOMMA
+                 | PROCEDIMIENTO ID LPARENTHESIS Params RPARENTHESIS variables INICIO Block INICIO DE PROCEDIMIENTO error
+            '''
+            self.print_sintactic_error(p, 'Error de sintaxis en procedimiento')
+
         def p_Func(p):
             '''
             Func : FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
             '''
             p[0] = self.join_result(p)
+
+        def p_Func_error(p):
+            '''
+            Func : error ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION error LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID error Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params error 2DOTS TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS error TIPO variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS error variables INICIO Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables error Block FIN DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block error DE FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN error FUNCION DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE error DOTCOMMA
+                 | FUNCION ID LPARENTHESIS Params RPARENTHESIS 2DOTS TIPO variables INICIO Block FIN DE FUNCION error
+            '''
+            self.print_sintactic_error(p, 'Error de sintaxis en funcion')
 
         def p_Block(p):
             '''
@@ -64,6 +99,13 @@ class sintactico:
                   |
             '''
             p[0] = self.join_result(p)
+
+        def p_Block_error(p):
+            '''
+            Block : Estatuto error Block
+            '''
+            self.print_sintactic_error(
+                p, 'Es necesario poner punto y coma cuando terminas un bloque')
 
         def p_Estatuto(p):
             '''
@@ -91,12 +133,33 @@ class sintactico:
             '''
             p[0] = self.join_result(p)
 
+        def p_Lee_error(p):
+            '''
+            Lee : error LPARENTHESIS RPARENTHESIS
+                | LEE error RPARENTHESIS
+                | LEE LPARENTHESIS error
+                | error LPARENTHESIS ID RPARENTHESIS
+                | LEE error ID RPARENTHESIS
+                | LEE LPARENTHESIS ID error
+            '''
+            self.print_sintactic_error(p, 'Sentencia lee mal escrita')
+
         def p_Imprimenl(p):
             '''
             Imprimenl : IMPRIMENL LPARENTHESIS RPARENTHESIS
                       | IMPRIMENL LPARENTHESIS GpoExp RPARENTHESIS
             '''
             p[0] = self.join_result(p)
+
+        def p_Imprimenl_error(p):
+            '''
+            Imprimenl : IMPRIMENL error RPARENTHESIS
+                      | IMPRIMENL LPARENTHESIS error
+                      | error LPARENTHESIS GpoExp RPARENTHESIS
+                      | IMPRIMENL error GpoExp RPARENTHESIS
+                      | IMPRIMENL LPARENTHESIS GpoExp error
+            '''
+            self.print_sintactic_error(p, 'Sentencia Imprimenl mal escrita')
 
         def p_Imprime(p):
             '''
@@ -105,12 +168,28 @@ class sintactico:
             '''
             p[0] = self.join_result(p)
 
+        def p_Imprime_error(p):
+            '''
+            Imprime : IMPRIME error RPARENTHESIS
+                    | IMPRIME LPARENTHESIS error
+                    | IMPRIME error GpoExp RPARENTHESIS
+                    | IMPRIME LPARENTHESIS GpoExp error
+            '''
+            self.print_sintactic_error(p, 'Sentencia Imprime mal escrita')
+
         def p_GpoExp(p):
             '''
             GpoExp : Exprlog
                    | Exprlog COMMA GpoExp
             '''
             p[0] = self.join_result(p)
+
+        def p_GpoExp_error(p):
+            '''
+            GpoExp : Exprlog error GpoExp
+            '''
+            self.print_sintactic_error(
+                p, 'Sentencia mal escrita es necesario poner ,')
 
         def p_Lproc(p):
             '''
@@ -364,9 +443,7 @@ class sintactico:
             p[0] = self.join_result(p)
 
         def p_error(p):
-            if p:
-                print("Syntax error in input!", p.type, p)
-            else:
+            if not p:
                 print('End of file reached')
 
         parser = yacc.yacc()
